ef154fdcadb39abf21964a7742e9beed667eb6a0
dmoriley <david.m.oriley@hotmail.com>
2018-02-15 18:38:09 -0500

    Functionality completed

--- src/Components/CurrencyConverter/CurrencyConverter.js
+++ src/Components/CurrencyConverter/CurrencyConverter.js
@@ -4,18 +4,37 @@ class CurrencyConverter extends HTMLElement {
     constructor() {
       // If you define a constructor, always call super() first as it is required by the CE spec.
       super();
-  
-      // Setup a click listener on <user-card>
-      this.addEventListener('click', e => {
-        this.hello();
-      });
-    }
-  
-    hello() {
-      console.log("Element was clicked!");
+
+      this.state = {
+          inputValue: '',
+          converted: '',
+          baseCurrency: 'CAD',
+          targetCurrency: 'USD',
+          showDisclaimer: false
+      };
+
+      //left blank for any base/target values to be added
+      this.conversionValues = {};
+
+      /* First attemp at event listening, they worked but better to put on the element itself i think
+        //set up listener for when the input textbox is changed  
+        this.addEventListener('keyup', e => {
+            e.stopPropagation();
+            this.updateConversion(e); //update the conversion number
+        });
+
+
+        //listen for a click event
+        this.addEventListener('click', e => {
+            e.stopPropagation();
+            if(e.path[0].id === 'disclaimer') { //check if the disclaimer Anchor was clicked
+                this.toggleDisclaimer();
+            }
+        });
+        */
     }
 
-     // Called when element is inserted in DOM
+    // Called when element is inserted in DOM
     connectedCallback() {
         const shadowRoot = this.attachShadow({mode: 'open'});
 
@@ -25,21 +44,154 @@ class CurrencyConverter extends HTMLElement {
         const instance = template.content.cloneNode(true);
         shadowRoot.appendChild(instance);
 
-        // // Extract the attribute user-id from our element. 
-        // // Note that we are going to specify our cards like: 
-        // // <user-card user-id="1"></user-card>
-        // const userId = this.getAttribute('user-id');
+        //get all the conversion data needed for the base's cad, usd and eur
+        this.getAllConversionData();
 
-        // // Fetch the data for that user Id from the API and call the render method with this data
-        // fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
-        //     .then((response) => response.text())
-        //     .then((responseText) => {
-        //         this.render(JSON.parse(responseText));
-        //     })
-        //     .catch((error) => {
-        //         console.error(error);
-        //     });
+        //listener for when input-currency-type is changed
+        this.shadowRoot.querySelector('#input-currency-type').addEventListener('change', e => {
+            this.updateBaseType(e.target.value);
+        });
+
+        //listener for when output-currency-type is changed
+        this.shadowRoot.querySelector('#output-currency-type').addEventListener('change', e => {
+            this.updateTargetType(e.target.value);
+        });
+
+        //listener for when disclaimer is toggled
+        this.shadowRoot.querySelector('#disclaimer').addEventListener('click', e => {
+            this.toggleDisclaimer();
+        });
+
+        //set up listener for when the input textbox is changed 
+        this.shadowRoot.querySelector('#input-currency').addEventListener('keyup', e => {
+            this.setConversionAmount(e); //update the conversion number
+        });
     }
+
+    /**
+     * Update the state of the component by passing an object with the corresponding values
+     * @param {*Object} val Object to update the state values
+     */
+    setState(val){
+        Object.assign(this.state, val);
+    }
+
+    /**
+     * Update the conversionValues of the component by passing an object with the corresponding values
+     * @param {*Object} val Object to update the conversionValues values
+     */
+    setConversionValues(val) {
+        Object.assign(this.conversionValues, val);
+    }
+  
+    /**
+     * Validate the input by limiting the input to only numbers with this pattern: /^\d{1,}(\.\d{0,6})?$/
+     * @param {*Event Object} e Event object passed from the element 
+     */
+    validateInput(e) {
+        return new Promise((res,rej) => {
+            let inputValue = e.path[0].value;
+
+            //backup check on user input to only be a number
+            //limited to 6 decimal places
+            if(!inputValue || inputValue.match(/^\d{1,}(\.\d{0,6})?$/)){
+                //this state isnt set if the pattern doesnt match preventing the user from enter letters, period first ect...
+                this.setState({inputValue});            
+                e.path[0].value = this.state.inputValue; //input value tied to component state
+                res(inputValue);
+            } else {
+                //need to update textbox to inputValue before additional input was added
+                e.path[0].value = this.state.inputValue;
+                rej('Invalid input');
+            }
+        });
+    }
+
+    /**
+     * Calculate the converted amount and update the value in the textbox
+     */
+    updateConversion() {
+        let converted = this.state.inputValue;
+        if(converted != '') {
+            converted = this.calculateConvertedAmount(this.state.baseCurrency,this.state.targetCurrency, parseFloat(this.state.inputValue));
+            this.setState({converted});
+        }
+        this.shadowRoot.querySelector('#output-currency').value = converted; //set value in output box
+    }
+
+    /**
+     * Update the conversion rate in the output box after validating the input 
+     * @param {*Event object} e Event object from the element
+     */
+    setConversionAmount(e) {
+        this.validateInput(e).then((res) => {
+            this.updateConversion();
+        }).catch((err) => {
+            console.log(err);
+        });
+
+    }
+
+     /**
+      * Toggle the disclaimer info from block to none
+      */
+    toggleDisclaimer() {
+        const current = this.shadowRoot.querySelector('#disclaimer-addition-info').style.display;
+        this.shadowRoot.querySelector('#disclaimer-addition-info').style.display = (current === 'block') ? 'none' : 'block';
+    }
+
+    /**
+     * Update the base currency type
+     * @param {*String} baseCurrency Currency type for the base
+     */
+    updateBaseType(baseCurrency) {
+        this.setState({baseCurrency});
+        this.updateConversion();
+    }
+
+    /**
+     * Update the target currency type
+     * @param {*String} targetCurrency Currency type for target
+     */
+    updateTargetType(targetCurrency) {
+        this.setState({targetCurrency});
+        this.updateConversion();
+    }
+
+    /**
+     * Get the conversion data from fixer.io for all possible bases. Possible bases are: CAD, USD, EUR
+     * I chose to get them all at once because of there being only 3 possible bases it would be more efficient to 
+     * grab them all at once instead of constantly querying the api. If there were alot more this might not be
+     * the most efficient route.
+     */
+    getAllConversionData() {
+        // fetch('https://api.fixer.io/latest?base=CAD&symbols=USD,EUR')
+        this.getConversionData({base:'CAD',targets:['USD','EUR']});
+        this.getConversionData({base:'USD',targets:['CAD','EUR']});
+        this.getConversionData({base:'EUR',targets:['USD','EUR']});
+        // setTimeout(() => {
+        //     console.log(typeof this.conversionValues.CAD.USD);
+        // },500)
+    }
+
+    /**
+     * Get the conversion data of a supplied base for the supplied targets and set to the conversionValues property.
+     * @param {*Object} ConversionObject Object with the following pattern {base: CAD, targets: ['USD','EUR']} Only one base and as many targets as you want. 
+     */
+    getConversionData({base, targets}) {
+        fetch(`https://api.fixer.io/latest?base=${base}&symbols=${targets.toString()}`)
+        .then(response => response.json()) //takes response stream and resolves to a string
+        .then(responseText => {
+            responseText.rates[base] = 1; //setup 1 to 1 conversion
+            this.setConversionValues({[base]: responseText.rates});
+        });
+    }
+
+    calculateConvertedAmount(base, target, value) {
+        return (value * this.conversionValues[base][target]).toFixed(2);
+    }
+
+
 
     render(userData) {
         // // Fill the respective areas of the card using DOM manipulation APIs
